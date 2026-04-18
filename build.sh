92a5d8b02109f951a3b4dc50bf7da9f81ddd3449
zyt-zsn <zyt_xl@163.com>
2026-04-18 20:55:52 +0800

    Add installation of jre and plantuml

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+set -xeo
+cat ~/plantuml.jar > /dev/null || wget https://github.com/plantuml/plantuml/releases/download/v1.2024.5/plantuml-gplv2-1.2024.5.jar -o ~/plantuml.jar
 emacs -Q --script build-site.el
